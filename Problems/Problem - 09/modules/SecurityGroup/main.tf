de508f13f6b048125e8cfd9fc8d2187d6fa3a3f4
Shravani Budharam <shravanibudharam25@gmail.com>
2026-06-25 20:16:53 +0530

    Create main.tf

---
+++ Problems/Problem - 09/modules/SecurityGroup/main.tf
@@ -0,0 +1,13 @@
+resource "aws_security_group" "allow_ssh" {
+  name = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id = var.vpc_id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
