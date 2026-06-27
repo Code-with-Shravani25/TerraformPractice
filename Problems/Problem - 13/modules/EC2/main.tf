6468d84c821803d15cc5e1b441d50ecd36bf85cf
Shravani Budharam <shravanibudharam25@gmail.com>
2026-06-28 02:07:09 +0530

    Create main.tf

---
+++ Problems/Problem - 13/modules/EC2/main.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "ec2" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+  key_name = "Demo"
+  subnet_id = var.subnet_id
+  vpc_security_group_ids = [var.security_group_id]
+}
